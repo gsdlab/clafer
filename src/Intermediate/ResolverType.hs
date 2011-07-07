7f3d0c7a402ea7a6ad13b6560984ea9f724eab86
kbak <kbak@gsd.uwaterloo.ca>
2011-07-06 22:08:22 -0400

    licence

--- src/Intermediate/ResolverType.hs
+++ src/Intermediate/ResolverType.hs
@@ -1,3 +1,22 @@
+{-
+ This file is part of the Clafer Translator (clafer).
+
+ Copyright (C) 2010 Kacper Bak <http://gsd.uwaterloo.ca/kbak>
+
+ clafer is free software: you can redistribute it and/or modify
+ it under the terms of the GNU Lesser General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ clafer is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU Lesser General Public License for more details.
+
+ You should have received a copy of the GNU Lesser General Public License
+ along with clafer. (See files COPYING and COPYING.LESSER.)  If not,
+ see <http://www.gnu.org/licenses/>.
+-}
 module Intermediate.ResolverType where
 
 import Data.Function
