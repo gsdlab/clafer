23863d762c5a73f537f4fdd51c1b205b5a9f8528
Jimmy Liang <paradoxical.reasoning@gmail.com>
2012-05-03 23:34:33 -0400

    Added scope analysis on constraints. Still needs work on 'greedy alorithm' and currently overestimates with multiple specialization (can and will be fixed).

--- src/Intermediate/ScopeAnalyzer.hs
+++ src/Intermediate/ScopeAnalyzer.hs
@@ -7,9 +7,10 @@ import Data.Map (Map)
 import qualified Data.Map as Map
 import Data.Maybe
 import Data.Ord
+import Front.Absclafer
 import Intermediate.Intclafer
 import Prelude hiding (exp)
-
+import Debug.Trace
 
 
 isReference = isOverlapping . super
@@ -33,10 +34,23 @@ scopeAnalysis IModule{mDecls = decls} =
     isConcrete' uid = isConcrete $ findClafer uid
     
     referenceAnalysis = foldl (analyzeReferences clafers) Map.empty decls
+    constraintAnalysis = analyzeConstraints constraints
     (subclaferMap, parentMap) = analyzeHierarchy clafers
     connectedComponents = analyzeDependencies clafers
     clafers = concatMap findClafers decls
+    constraints = concatMap findConstraints decls
     findClafer uid = fromJust $ find (isEqClaferId uid) clafers
+    
+    lowCard clafer =
+        max low constraintLow'
+        where
+        low = fst $ fromJust $ card clafer
+        constraintLow = Map.findWithDefault 0 (uid clafer) constraintAnalysis
+        -- Don't exceed the stated maximum
+        constraintLow' = min' constraintLow $ snd $ fromJust $ card clafer
+        
+        min' a (ExIntegerNum b) = min a b
+        min' a ExIntegerAst = a
     
     analyzeComponent analysis component =
         case flattenSCC component of
@@ -56,9 +70,8 @@ scopeAnalysis IModule{mDecls = decls} =
         where
         scope
             | isAbstract clafer  = sum subclaferScopes
-            | otherwise          = parentScope * lowCard
+            | otherwise          = parentScope * lowCard clafer
         
-        lowCard = fst $ fromJust $ card clafer
         subclaferScopes = map (findOrError " subclafer scope not found" analysis) $ filter isConcrete' subclafers
         parentScope  =
             case parent of
@@ -80,6 +93,76 @@ analyzeReferences clafers analysis (IEClafer clafer) =
 analyzeReferences _ analysis _ = analysis
 
 
+analyzeConstraints :: [PExp] -> Map String Integer
+analyzeConstraints constraints =
+    foldr analyzeConstraint Map.empty constraints
+    where
+    -- Only considers how quantifiers affect scope. Other types of constraints are not considered.
+    -- Constraints of the type [some path1.path2] or [no path1.path2], etc.    
+    analyzeConstraint PExp{exp = IDeclPExp{quant = quant, oDecls = [], bpexp = bpexp}} analysis =
+        case quant of
+            IOne  -> analysis'
+            ISome -> analysis'
+            _     -> analysis -- The other quantifiers allow zero so skip.
+        where
+        analysis' = foldr atLeastOne analysis (top : follow)
+        top : follow = dropThisAndParent $ unfoldJoins bpexp
+        atLeastOne = Map.insertWith max `flip` 1
+        
+    -- Constraints of the type [all disj a : path1.path2] or [some b : path3.path4], etc.
+    analyzeConstraint PExp{exp = IDeclPExp{oDecls = decls}} analysis =
+        foldr analyzeDecl analysis decls
+    analyzeConstraint _ analysis = analysis
+
+    analyzeDecl IDecl{isDisj = isDisj, decls = decls, body = body} analysis =
+        insert minScope top $ foldr (insert 1) analysis follow
+        where
+        -- Take the first element in the path, and change its effective lower cardinality.
+        -- Can overestimate the scope.
+        top : follow = dropThisAndParent $ unfoldJoins body
+        -- "disj a;b;c" implies at least 3 whereas "a;b;c" implies at least one.
+        minScope = if isDisj then fromIntegral $ length decls else 1
+        insert s = Map.insertWith max `flip` s
+        
+        {-
+         - abstract Z
+         -   C *
+         -     D : integer *
+         -
+         - A : Z
+         -   B : integer
+         -   [some disj a;b;c;d : D | a = 1 && b = 2 && c = 3 && d = B]
+         -}
+         -- Need at least 4 D's per A.
+         -- Either
+         -- a) Make the effective lower cardinality of C=4 and D=1
+         -- b) Make the effective lower cardinality of C=1 and D=4
+         -- c) Some other combination.
+         -- Choose b.
+         -- A greedy algorithm that starts from the lowest child progressing upwards.
+         
+        {-
+         - abstract Z
+         -   C *
+         -     D : integer 3..*
+         -
+         - A : Z
+         -   B : integer
+         -   [some disj a;b;c;d : D | a = 1 && b = 2 && c = 3 && d = B]
+         -}
+         -- The algorithm we do is greedy so it will chose D=3.
+         -- However, it still needs more D's so it will choose C=2
+         -- C=2, D=3
+         -- This might not be optimum since now the scope allows for 6 D's.
+         -- A better solution might be C=2, D=2.
+         -- Well too bad, we are using the greedy algorithm.
+        
+    -- The each child has at most one parent. No matter what the path in a quantifier
+    -- looks like, we ignore the parent parts.
+    dropThisAndParent = dropWhile (== "parent") . dropWhile (== "this")
+
+
+analyzeDependencies :: [IClafer] -> [SCC String]
 analyzeDependencies clafers = connComponents
     where
     connComponents  = stronglyConnComp [(key, key, depends) | (key, depends) <- dependencyGraph]
@@ -127,8 +210,15 @@ directSuper clafers clafer =
     
 
 -- Finds all ancestors
+findClafers :: IElement -> [IClafer]
 findClafers (IEClafer clafer) = clafer : concatMap findClafers (elements clafer)
 findClafers _ = []
+
+
+-- Find all constraints
+findConstraints :: IElement -> [PExp]
+findConstraints IEConstraint{cpexp = c} = [c]
+findConstraints (IEClafer clafer) = concatMap findConstraints (elements clafer)
 
 
 -- Finds all the direct ancestors (ie. children)
@@ -137,3 +227,19 @@ childClafers clafer =
     where
     asClafer (IEClafer clafer) = Just clafer
     asClafer _ = Nothing
+    
+    
+-- Unfold joins
+-- If the expression is a tree of only joins, then this function will flatten
+-- the joins into a list.
+-- Otherwise, returns an empty list.
+unfoldJoins :: PExp -> [String]
+unfoldJoins pexp =
+    fromMaybe [] $ unfoldJoins' pexp
+    where
+    unfoldJoins' PExp{exp = (IFunExp "." args)} =
+        return $ args >>= unfoldJoins
+    unfoldJoins' PExp{exp = IClaferId{sident = sident}} =
+        return $ [sident]
+    unfoldJoins' _ =
+        fail "not a join"
