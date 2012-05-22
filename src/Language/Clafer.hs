105f9612801d67386ada5bfa191ae4c849e57d08
Michal Antkiewicz <mantkiew@gsd.uwaterloo.ca>
2012-05-22 12:13:39 -0400

    split into cmd app and Language.Clafer library (2)
    moved more functions into Clafer.hs: generate, analyze, desugar, etc.

--- src/Language/Clafer.hs
+++ src/Language/Clafer.hs
@@ -3,28 +3,20 @@ module Language.Clafer (
                         myLexer,  -- don't want to export (addFragment)
                         resolveLayout,-- don't want to export (addFragment)
                         resLayout, -- don't want to export (addFragment)
-                        desugarModule,-- don't want to export (addFragment)
-                        findDupModule,-- don't want to export 
-                        allUnique,-- don't want to export 
-                        resolveModule,-- don't want to export 
-                        transModule,-- don't want to export 
-                        optimizeModule,-- don't want to export 
-                        genModule,-- don't want to export 
-                        genXmlModule,-- don't want to export 
-                        sugarModule,-- don't want to export 
-                        Print, 
-                        printTree, 
+                        desugar,
+						analyze,
+						generate,
                         Token,
                         pModule,
-                        astrModule,-- don't want to export 
                         Module,
                         claferIRXSD,
-						mapModule,
                         module Front.ErrM,
-                        module Generator.Stats,
                         module ClaferArgs,
-                        module Common                        
+						voidf               
 ) where
+
+import Data.Maybe
+import Control.Monad
 
 import Common
 import Front.ErrM
@@ -44,6 +36,58 @@ import Generator.Alloy
 import Generator.Xml
 import Generator.Schema
 import Generator.Stats
+
+desugar args tree = do
+--  conPutStrLn args "[Desugaring]"
+  let dTree = desugarModule $ mapModule tree
+  return dTree
+  -- writeFile (f ++ ".des") $ printTree $
+  --  sugarModule dTree
+
+analyze args tree = do
+  let dTree' = findDupModule args tree
+  let au = allUnique dTree'
+  let args' = args{skip_resolver = Just $ au && (fromJust $ skip_resolver args)}
+--  conPutStrLn args "[Resolving]"
+  let (rTree, genv) = resolveModule args' dTree'
+--  conPutStrLn args "[Transforming]"
+  let tTree = transModule rTree
+--  conPutStrLn args "[Optimizing]"
+  return $ (optimizeModule args' (tTree, genv), genv, au)
+  -- writeFile (f ++ ".ana") $ printTree $
+  --  sugarModule oTree
+
+  
+generate f args (oTree, genv, au) = do
+--  conPutStrLn args "[Generating Code]"
+  let stats = showStats au $ statsModule oTree
+  when (not $ fromJust $ no_stats args) $ putStrLn stats
+--  conPutStrLn args "[Saving File]"
+  let alloyCode = genModule args (astrModule oTree, genv)
+  let addCommentStats = if fromJust $ no_stats args then const else addStats
+  let (ext, code) = case (fromJust $ mode args) of
+                      Alloy   -> ("als", addCommentStats (fst alloyCode) stats)
+                      Alloy42 -> ("als", addCommentStats (fst alloyCode) stats)
+                      Xml     -> ("xml", genXmlModule oTree)
+                      Clafer  -> ("des.cfr", printTree $ sugarModule oTree)
+  let f' = f ++ "." ++ ext
+  if fromJust $ console_output args then putStrLn code else writeFile f' code
+  let mf = f ++ "." ++ "map"
+  when (fromJust $ alloy_mapping args) $ writeFile mf $ show $ snd alloyCode
+  return f'
+  
+addStats code stats = "/*\n" ++ stats ++ "*/\n" ++ code
+
+
+showStats au (Stats na nr nc nconst ngoals sgl) =
+  unlines [ "All clafers: " ++ (show (na + nr + nc)) ++ " | Abstract: " ++ (show na) ++ " | Concrete: " ++ (show nc) ++ " | References: " ++ (show nr)          , "Constraints: " ++ show nconst
+          , "Goals: " ++ show ngoals  
+          , "Global scope: " ++ showInterval sgl
+          , "All names unique: " ++ show au]
+
+
+showInterval (n, -1) = show n ++ "..*"
+showInterval (n, m) = show n ++ ".." ++ show m
 
 claferIRXSD :: [Char]
 claferIRXSD = Generator.Schema.xsd
--- src/clafer.hs
+++ src/clafer.hs
@@ -38,25 +38,6 @@ import System.FilePath.Posix
 
 import Language.Clafer
 
---import Common
---import ClaferArgs
---import Front.Lexclafer
---import Front.Parclafer
---import Front.Printclafer
---import Front.Absclafer hiding (Clafer)
---import Front.LayoutResolver
---import Front.ErrM
---import Front.Mapper
---import Intermediate.Desugarer
---import Intermediate.Resolver
---import Intermediate.StringAnalyzer
---import Intermediate.Transformer
---import Optimizer.Optimizer
---import Generator.Stats
---import Generator.Alloy
---import Generator.Xml
---import Generator.Schema
-
 type ParseFun = [Token] -> Err Module
 
 type VerbosityL = Int
@@ -100,60 +81,8 @@ run v p args input = do
                           f' <- generate f args oTree
                           when (fromJust $ validate args) $ runValidate args f'
 
-desugar args tree = do
---  conPutStrLn args "[Desugaring]"
-  let dTree = desugarModule $ mapModule tree
-  return dTree
-  -- writeFile (f ++ ".des") $ printTree $
-  --  sugarModule dTree
-
-analyze args tree = do
-  let dTree' = findDupModule args tree
-  let au = allUnique dTree'
-  let args' = args{skip_resolver = Just $ au && (fromJust $ skip_resolver args)}
---  conPutStrLn args "[Resolving]"
-  let (rTree, genv) = resolveModule args' dTree'
---  conPutStrLn args "[Transforming]"
-  let tTree = transModule rTree
---  conPutStrLn args "[Optimizing]"
-  return $ (optimizeModule args' (tTree, genv), genv, au)
-  -- writeFile (f ++ ".ana") $ printTree $
-  --  sugarModule oTree
-
-
-generate f args (oTree, genv, au) = do
---  conPutStrLn args "[Generating Code]"
-  let stats = showStats au $ statsModule oTree
-  when (not $ fromJust $ no_stats args) $ putStrLn stats
---  conPutStrLn args "[Saving File]"
-  let alloyCode = genModule args (astrModule oTree, genv)
-  let addCommentStats = if fromJust $ no_stats args then const else addStats
-  let (ext, code) = case (fromJust $ mode args) of
-                      Alloy   -> ("als", addCommentStats (fst alloyCode) stats)
-                      Alloy42 -> ("als", addCommentStats (fst alloyCode) stats)
-                      Xml     -> ("xml", genXmlModule oTree)
-                      Clafer  -> ("des.cfr", printTree $ sugarModule oTree)
-  let f' = f ++ "." ++ ext
-  if fromJust $ console_output args then putStrLn code else writeFile f' code
-  let mf = f ++ "." ++ "map"
-  when (fromJust $ alloy_mapping args) $ writeFile mf $ show $ snd alloyCode
-  return f'
 
 conPutStrLn args s = when (not $ fromJust $ console_output args) $ putStrLn s
-
-
-addStats code stats = "/*\n" ++ stats ++ "*/\n" ++ code
-
-
-showStats au (Stats na nr nc nconst ngoals sgl) =
-  unlines [ "All clafers: " ++ (show (na + nr + nc)) ++ " | Abstract: " ++ (show na) ++ " | Concrete: " ++ (show nc) ++ " | References: " ++ (show nr)          , "Constraints: " ++ show nconst
-          , "Goals: " ++ show ngoals  
-          , "Global scope: " ++ showInterval sgl
-          , "All names unique: " ++ show au]
-
-
-showInterval (n, -1) = show n ++ "..*"
-showInterval (n, m) = show n ++ ".." ++ show m
 
 runValidate args fo = do
   let path = (fromJust $ tooldir args) ++ "/"
