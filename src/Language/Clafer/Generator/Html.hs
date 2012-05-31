79670cbe0220e126734faaf203386558625a8f84
walkerc <chris.walker@sympatico.ca>
2012-05-31 16:57:18 -0400

    Revised Generator.Html to have a similar structure to Front.PrintClafer

--- src/Language/Clafer/Generator/Html.hs
+++ src/Language/Clafer/Generator/Html.hs
@@ -34,40 +34,82 @@ data Tag = Html | Text deriving (Eq,Ord,Show) --this will be used later (see abo
 
 genHtml (Module [])     = ""
 genHtml (Module ((ElementDecl x):xs)) = (genHtmlh x 0) ++ (genHtml $ Module xs)
+genHtml _ = "genHtml encountered an unknown pattern"
 -- For some reason it is outputting too much indentation. I don't think it will be a large problem, though
 genHtmlh (Subclafer (Clafer abstract gCard ident super card init (ElementsList elements))) indent =
   (printIndent indent) ++
-    (unwords [printAbstract abstract, printGCard gCard, printId ident, printSuper super,
+    (unwords [printAbstract abstract, printGCard gCard, printPosIdent ident, printSuper super,
     printCard card, printInit init]) ++ "\n" ++ (concatMap (\x -> genHtmlh x (indent + 1)) elements)
-genHtmlh _ _ = "END"
+--add a pattern to match constraints
+genHtmlh (Subconstraint constraints) indent =
+  (printIndent indent) ++ "[ "  ++ (printExp constraints) ++ " ]\n"
+genHtmlh _ _ = "END" --will become "", but it is "END" for testing purposes
 
 -- the "Data Omitted" Lines are for me to quickly identify when pattern matching fails, but with useful info
-printIndent indent = replicate (indent * 2) ' '
 
 printAbstract Abstract = "abstract"
 printAbstract AbstractEmpty = ""
 printAbstract x = "Abstract Omitted"
 
-printGCard (GCardInterval (NCard (PosInteger (pos, num)) ExIntegerAst)) = ""
+printGCard (GCardInterval ncard) = printNCard ncard
+printGCard GCardEmpty = ""
+printGCard GCardXor = "xor"
+printGCard GCardOr = "or"
+printGCard GCardMux = "mux"
+printGCard GCardOpt = "opt"
 printGCard x = "GCardInterval Omitted"
 
-printId (PosIdent (pos, id))
-  | validPos pos = (tail $ dropWhile (\x -> x /= '_') id) -- drops the UID_ from UID_ID
-  | otherwise    = "" 
-printId x = "Id Omitted"
+printNCard (NCard (PosInteger (pos, num)) ExIntegerAst)
+  | validPos pos && num == "0" = ""
+  | validPos pos               = num ++ "..*"
+  | otherwise                  = ""
+printNCard (NCard (PosInteger (pos1, int1)) (ExIntegerNum (PosInteger (pos2, int2))))
+  | validPos pos1 && validPos pos2 = int1 ++ ".." ++ int2
+  | otherwise = ""
+printNCard _ = "NCard Omitted"
 
-printSuper (SuperSome SuperColon (ClaferId (Path [ModIdIdent (PosIdent (pos,id))])))
-  | validPos pos = ": " ++ id
-  | otherwise    = ""
+printName (Path modids) = unwords $ map printModId modids
+printName _             = "Name Omitted"
+
+printModId (ModIdIdent posident) = printPosIdent posident
+
+printPosIdent (PosIdent (pos, id))
+  | id == "clafer" = ""
+  | validPos pos   = dropUid id
+  | otherwise      = ""
+
+printSuper SuperEmpty = ""
+printSuper (SuperSome superHow setExp) = let str = printSetExp setExp in
+  if str /= ""
+  then printSuperHow superHow ++ str
+  else ""
 printSuper x = "Super Omitted"
 
-printCard (CardInterval (NCard (PosInteger (pos1,num1)) (ExIntegerNum (PosInteger (pos2,num2)))))
-  | validPos pos1 = num1 ++ ".." ++ num2
-  | otherwise     = ""
+printSuperHow SuperColon  = ":"
+printSuperHow SuperArrow  = "->"
+printSuperHow SuperMArrow = "->>"--non-exhaustive
+printSuperHow _           = "SuperHow Omitted"
+
+printCard (CardInterval nCard) = printNCard nCard
+{-
+printCard (CardInterval (NCard (PosInteger (pos, num)) ExIntegerAst))
+  | validPos pos && num == "0" = ""
+  | validPos pos               = num ++ "..*"
+  | otherwise                  = ""-}
 printCard x = "Cardinality Omitted"
 
+printInit InitEmpty = ""
 printInit x = "Initialization Omitted"
+
+printExp _                  = "Exp Omitted"
+
+printSetExp (ClaferId name) = printName name
+printSetExp _               = "setExp Omitted"
+
+printIndent indent = replicate (indent * 2) ' '
 
 validPos (row, col)
   | row >= 0 && col >= 0 = True
   | otherwise            = False
+
+dropUid id = tail $ dropWhile (\x -> x /= '_') id
