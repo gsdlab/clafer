288bd33bc61dbc10024903c4af9ad0b3ed2a9e7e
Rafael Olaechea <rolaechea@gsd.uwaterloo.ca>
2012-06-24 18:23:16 -0400

    updated dataset

--- run_clafer_moo_demo.sh
+++ run_clafer_moo_demo.sh
@@ -1,4 +1,4 @@
 ./clafer -n spl_configurator/dataset/telematics_multi_objective_optimization.cfr
 ./clafer --mode=xml -n spl_configurator/dataset/telematics_multi_objective_optimization.cfr
-python spl_configurator/py_src/SPL_Configurator_clafer_generator.py --property=performance --sparseintegers spl_configurator/dataset/telematics_multi_objective_optimization.xml >> spl_configurator/dataset/telematics_multi_objective_optimization.als 
+python spl_configurator/py_src/SPL_Configurator_clafer_generator.py --property=cost --sparseintegers spl_configurator/dataset/telematics_multi_objective_optimization.xml >> spl_configurator/dataset/telematics_multi_objective_optimization.als 
 java -jar /Users/rafaelolaechea/Downloads/alloy_moo.jar ~/clafer/spl_configurator/dataset/telematics_multi_objective_optimization.als
