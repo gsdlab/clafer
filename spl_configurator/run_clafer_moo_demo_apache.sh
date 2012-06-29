812ac58f8dc9ac8699ada6ec6a77170c21673d07
Rafael Olaechea <rolaechea@gsd.uwaterloo.ca>
2012-06-29 13:49:14 -0400

    Changed path in run_xx_demo.sh files to spl_configurator, before merging to develop

--- spl_configurator/run_clafer_moo_demo_apache.sh
+++ spl_configurator/run_clafer_moo_demo_apache.sh
@@ -1,4 +1,4 @@
-./clafer -n spl_configurator/dataset/apache.cfr
-./clafer --mode=xml -n spl_configurator/dataset/apache.cfr
-python spl_configurator/py_src/SPL_Configurator_clafer_generator.py --property=performance --sparseintegers spl_configurator/dataset/apache.xml >> spl_configurator/dataset/apache.als 
+../clafer --nr dataset/apache.cfr
+../clafer --mode=xml --nr dataset/apache.cfr
+python py_src/SPL_Configurator_clafer_generator.py --property=performance --sparseintegers dataset/apache.xml >> dataset/apache.als 
 java -jar /Users/rafaelolaechea/Downloads/alloy_moo.jar ~/clafer/spl_configurator/dataset/apache.als
