eac9e7cdb36f57731f4d53b27a7037a97cf291ca
Rafael Olaechea <rolaechea@gsd.uwaterloo.ca>
2012-07-01 00:09:10 -0400

    Added Support for multi-objective optimization

--- spl_configurator/run_clafer_moo_demo_sqlite.sh
+++ spl_configurator/run_clafer_moo_demo_sqlite.sh
@@ -1,4 +1,4 @@
 ../clafer --nr dataset/sqlite_scaled_tiny.cfr
 ../clafer --mode=xml --nr dataset/sqlite_scaled_tiny.cfr
-python py_src/SPL_Configurator_clafer_generator.py --property=footprint --sparseintegers dataset/sqlite_scaled_tiny.xml >> dataset/sqlite_scaled_tiny.als 
+python py_src/SPL_Configurator_clafer_generator.py --sparseintegers dataset/sqlite_scaled_tiny.xml >> dataset/sqlite_scaled_tiny.als 
 java -jar /Users/rafaelolaechea/Downloads/alloy_moo.jar ~/clafer/spl_configurator/dataset/sqlite_scaled_tiny.als
