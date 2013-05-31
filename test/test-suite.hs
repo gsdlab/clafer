0b878d1a4e6640e629d2f4277fef311cb0f77e91
Luke Brown <Luke.michael.brown@gmail.com>
2013-05-31 14:20:32 -0400

    Added test infrastructure

--- test/test-suite.hs
+++ test/test-suite.hs
@@ -1,15 +1,13 @@
 {-# OPTIONS_GHC -XTemplateHaskell #-}
-import Data.List
-import Data.Map 
+import Language.Clafer
+import Language.ClaferT
+import Language.Clafer.Css
 import Test.Framework
 import Test.Framework.TH
 import Test.Framework.Providers.HUnit
 import Test.Framework.Providers.QuickCheck2
 import Test.HUnit
 import Test.QuickCheck
-import Language.Clafer
-import Language.ClaferT
-import Language.Clafer.ClaferArgs
 
 tg_testsuite = $(testGroupGenerator)
 
