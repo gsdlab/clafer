b302337865cfb963943a890d16e90d79254461ee
Luke Brown <Luke.michael.brown@gmail.com>
2013-06-06 08:26:43 -0400

    Fixed issue in test case where files could not have '.' in name as well as also reading ".dst.cfr" files

--- test/test-suite.hs
+++ test/test-suite.hs
@@ -1,4 +1,25 @@
 {-# OPTIONS_GHC -XTemplateHaskell #-}
+{-
+ Copyright (C) 2013 Luke Brown <http://gsd.uwaterloo.ca>
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy of
+ this software and associated documentation files (the "Software"), to deal in
+ the Software without restriction, including without limitation the rights to
+ use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ of the Software, and to permit persons to whom the Software is furnished to do
+ so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+-}
 import qualified Data.List as List
 import Data.Monoid
 import Language.Clafer
@@ -17,14 +38,13 @@ tg_testsuite = $(testGroupGenerator)
 
 main = defaultMain[tg_testsuite]
 
-getClafer file = do
-					clafer <- readFile file
-					return clafer
-
+getClafersHelper file = if (file == "dst.cfr") then True else (txe == "rfc") && (takeWhile (/='.') (reverse eman) /= "tsd")
+						where 
+							(txe,dot:eman) = span (/='.') (reverse file)
 getClafers dir = do
 					files <- getDirectoryContents dir
-					let clafers = List.filter (\f -> (dropWhile (/='.') f) == "cfr") files
-					mapM getClafer clafers
+					let clafers = List.filter getClafersHelper files
+					mapM readFile clafers
 
 positiveClafers = do
 					clafers <- getClafers "test/positive"
