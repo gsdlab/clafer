1e55ada8a039f32bea90527bfee2581ce4a0e2a2
Luke Brown <Luke.michael.brown@gmail.com>
2013-07-26 08:27:14 -0400

    Added Example test suite from SandBox

--- test/Suite/Positive.hs
+++ test/Suite/Positive.hs
@@ -74,3 +74,8 @@ case_nonempty_Cards = do
 		isEmptyCard (IRClafer (IClafer{cinPos=(Span (Pos l c) _), card = Nothing})) = "Line " ++ show l ++ " column " ++ show c ++ "\n"
 		isEmptyCard (IRClafer (IClafer{cinPos=(PosSpan _ (Pos l c) _), card = Nothing})) = "Line " ++ show l ++ " column " ++ show c ++ "\n"
 		isEmptyCard	_ = ""
+
+case_stringEqual :: Assertion
+case_stringEqual = do
+	let strMap = stringMap $ fromRight $ compileOneFragment defaultClaferArgs "A\n    text1 : string = \"some text\"\n    text2 : string = \"some text\""
+	(Map.size strMap) == 1 @? "Error string's assigned to differnet numbers!"
