94ec9958c638e8253422a8496e2bc6864cd3ef68
Michal Antkiewicz <mantkiew@gsd.uwaterloo.ca>
2014-06-25 13:24:01 -0400

    fixed the bug whereby union of strings was getting concatinated

--- src/Language/Clafer/Intermediate/StringAnalyzer.hs
+++ src/Language/Clafer/Intermediate/StringAnalyzer.hs
@@ -22,39 +22,37 @@
 -}
 module Language.Clafer.Intermediate.StringAnalyzer where
 
-import Data.Map (Map)
 import Data.Tuple
 import qualified Data.Map as Map
 import Control.Monad.State
 
-import Language.Clafer.Common
 import Language.Clafer.Intermediate.Intclafer
 
 -- | Convert all strings into ints for analysis
 -- | Return an inverse map from ints back to strings
-astrModule :: IModule -> (IModule, Map Int String)
+astrModule :: IModule -> (IModule, Map.Map Int String)
 astrModule imodule = (imodule{_mDecls = decls''}, flipMap strMap')
   where
     decls' = _mDecls imodule
     (decls'', strMap') = runState (mapM astrElement decls') Map.empty
 
-    flipMap :: Map String Int -> Map Int String
+    flipMap :: Map.Map String Int -> Map.Map Int String
     flipMap = Map.fromList . map swap . Map.toList
 
 
-astrClafer :: MonadState (Map String Int) m => IClafer -> m IClafer
+astrClafer :: MonadState (Map.Map String Int) m => IClafer -> m IClafer
 astrClafer (IClafer s isAbs gcrd ident' uid' super' crd gCard es) =
     IClafer s isAbs gcrd ident' uid' super' crd gCard `liftM` mapM astrElement es
 
 
 -- astrs single subclafer
-astrElement :: MonadState (Map String Int) m => IElement -> m IElement
+astrElement :: MonadState (Map.Map String Int) m => IElement -> m IElement
 astrElement x = case x of
   IEClafer clafer -> IEClafer `liftM` astrClafer clafer
   IEConstraint isHard' pexp -> IEConstraint isHard' `liftM` astrPExp pexp
   IEGoal isMaximize' pexp -> IEGoal isMaximize' `liftM` astrPExp pexp
 
-astrPExp :: MonadState (Map String Int) m => PExp -> m PExp
+astrPExp :: MonadState (Map.Map String Int) m => PExp -> m PExp
 astrPExp (PExp (Just TString) pid' pos' exp') =
     PExp (Just TInteger) pid' pos' `liftM` astrIExp exp'
 astrPExp (PExp t pid' pos' (IFunExp op' exps')) = PExp t pid' pos' `liftM`
@@ -63,23 +61,11 @@ astrPExp (PExp t pid' pos' (IDeclPExp quant' oDecls' bpexp')) = PExp t pid' pos'
                               (IDeclPExp quant' oDecls' `liftM` (astrPExp bpexp'))
 astrPExp x = return x
 
-astrIExp :: MonadState (Map String Int) m => IExp -> m IExp
+astrIExp :: MonadState (Map.Map String Int) m => IExp -> m IExp
 astrIExp x = case x of
-  IFunExp op' _ -> if op' == iUnion
-                     then astrIExp $ concatStrExp x else return x                    
+  IFunExp op' exps' -> IFunExp op' `liftM` mapM astrPExp exps'
   IStr str -> do
     modify (\e -> Map.insertWith (flip const) str (Map.size e) e)
     st <- get
-    --lift $ tell $ Map.singleton (toInteger $ (Map.!) st str) str 
     return $  (IInt $ toInteger $ (Map.!) st str)
-     
   _ -> return x
-
-
-concatStrExp :: IExp -> IExp
-concatStrExp x = case x of
-  IFunExp _ exps' -> IStr $ s0 ++ s1 
-    where
-    ((IStr s0):(IStr s1):_) = map concatStrExp $ map (Language.Clafer.Intermediate.Intclafer._exp) exps'
-  IStr _ -> x
-  _ -> x
