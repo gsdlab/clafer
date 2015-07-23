a212690718e4a91240671d23a25824d3a2979330
Michal Antkiewicz <mantkiew@gsd.uwaterloo.ca>
2015-07-23 14:49:10 -0400

    remove accidentally added test outputs

--- test/positive/negative.js
+++
@@ -1,8 +0,0 @@
-defaultScope(1);
-intRange(-8, 7);
-stringLength(16);
-
-c0_z = Clafer("c0_z").withCard(1, 1);
-c0_y = c0_z.addChild("c0_y").withCard(1, 1);
-c0_y.refTo(Int);
-c0_z.addConstraint(equal(joinRef(join($this(), c0_y)), constant(-1)));
--- test/positive/simple-arithmetic.py
+++
@@ -1,6 +0,0 @@
-from jsir.IR import *
-
-c0_A = Clafer("c0_A").withCard(1, 1);
-c0_A.addConstraint(equal(mul(add(constant(1), constant(2)), constant(3)), constant(9)));
-defaultScope(1);
-stringLength(16);
